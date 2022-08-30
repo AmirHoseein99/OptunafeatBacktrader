a0569a1a83656ba67bd74e986eacbd7bca7db4fb
AmirHoseein99 <amirimani491@gmail.com>
2022-08-30 17:22:46 +0430

    added params to strategy

--- backtraderWithParams.py
+++ backtraderWithParams.py
@@ -8,6 +8,10 @@ import pandas as pd
 import backtrader as bt
 from datetime import datetime
 
+
+PROFIT_LOSS_DICT = {}
+MAXDRAWDOWN = 0
+
 AUD_MID_PATH = r"datas/AUD_USD/MID/AUD_USD_MID.csv"
 EUR_MID_PATH = r"datas/EUR_USD/MID/EUR_USD_MID.csv"
 GBP_MID_PATH = r"datas/GBP_USD/MID/GBP_USD_MID.csv"
@@ -15,35 +19,17 @@ GBP_MID_PATH = r"datas/GBP_USD/MID/GBP_USD_MID.csv"
 
 class MyStrategy(bt.Strategy):
     params = (
-        ('eur_stoploss', 3000),
-        ('eur_takeprofit', 3000),
-        ('eur_open_count', 10),
-        ('eur_close_count', 5),
-        ('aud_stoploss', 3000),
-        ('aud_takeprofit', 3000),
-        ('aud_open_count', 10),
-        ('aud_close_count', 5),
-        ('gbp_stoploss', 3000),
-        ('gbp_takeprofit', 3000),
-        ('gbp_open_count', 10),
-        ('gbp_close_count', 5),
+        ('strat_params', None),
     )
 
     def __init__(self):
+        if self.params != None:
+            for name, val in self.params.strat_params.items():
+                print(name, val)
+                setattr(self.params, name, val)
 
         self.counter = -1
         self.order = None
-        self.eur = self.datas[0]
-        self.aud = self.datas[2]
-        self.gbp = self.datas[1]
-
-        self.close_eur = self.datas[0].close
-        self.close_aud = self.datas[2].close
-        self.close_gbp = self.datas[1].close
-
-        self.eur_pos = None
-        self.aud_pos = None
-        self.gbp_pos = None
 
     def log(self, txt, dt=None):
         ''' Logging function for this strategy'''
@@ -58,7 +44,7 @@ class MyStrategy(bt.Strategy):
         if order.status in [order.Completed]:
             if order.isbuy():
                 self.log(
-                    f'BUY EXECUTED on {order.data._name}, Price: %.5f, Size: %.2f, Position = %.2f' %
+                    f'BUY EXECUTED on {order.data._name}, Price: %.4f, Size: %.2f, Position = %.2f' %
                     (
                         order.executed.price,
                         order.executed.size,
@@ -68,7 +54,7 @@ class MyStrategy(bt.Strategy):
             else:
                 # Sell
                 self.log(
-                    f'SELL EXECUTED on {order.data._name}, Price: %.5f, Size: %.2f, Position = %.2f' %
+                    f'SELL EXECUTED on {order.data._name}, Price: %.4f, Size: %.2f, Position = %.2f' %
                     (
                         order.executed.price,
                         order.executed.size,
@@ -76,162 +62,153 @@ class MyStrategy(bt.Strategy):
                     )
                 )
         elif order.status in [order.Canceled]:
-            self.log(
-                f'{order.price} ON  {order.data._name} Order Canceleds ')
+            # self.log(
+            pass
+            #     f'{order.price} ON  {order.data._name} Order Canceleds ')
 
         self.order = None
 
-    def get_eur_limitprice(self, close_price):
-        profit_per_one = self.p.eur_takeprofit / 300000
-        return close_price + profit_per_one
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+        if bt.num2date(trade.data.datetime[0]).date().__str__() in PROFIT_LOSS_DICT:
+            PROFIT_LOSS_DICT[bt.num2date(trade.data.datetime[0]).date().__str__()
+                             ] += trade.pnl
+        else:
+            PROFIT_LOSS_DICT[bt.num2date(trade.data.datetime[0]).date().__str__()
+                             ] = trade.pnl
 
-    def get_eur_stopprice(self, close_price):
-        loss_per_one = self.p.eur_stoploss / 300000
-        return close_price + loss_per_one
+        self.log('OPERATION PROFIT, GROSS %.2f' % trade.pnl)
 
-    def get_aud_limitprice(self, close_price):
-        profit_per_one = self.p.aud_takeprofit / 300000
-        return close_price + profit_per_one
+    def get_limitprice(self, close_price, d_name):
+        if d_name == "EUR":
+            take_profit = self.p.eur_takeprofit
+        elif d_name == "AUD":
+            take_profit = self.p.aud_takeprofit
+        else:
+            take_profit = self.p.gbp_takeprofit
+        profit_per_one = take_profit / 300000
+        limitprice = close_price + profit_per_one
+        return limitprice
 
-    def get_aud_stopprice(self, close_price):
-        loss_per_one = self.p.aud_stoploss / 300000
-        return close_price + loss_per_one
+    def get_stopprice(self, close_price, d_name):
+        if d_name == "EUR":
+            stoploss = self.p.eur_stoploss
+        elif d_name == "AUD":
+            stoploss = self.p.aud_stoploss
+        else:
+            stoploss = self.p.gbp_stoploss
+        loss_per_one = stoploss / 300000
+        stopprice = close_price - loss_per_one
+        return stopprice
 
-    def get_gbp_limitprice(self, close_price):
-        profit_per_one = self.p.gbp_takeprofit / 300000
-        return close_price + profit_per_one
+    def check_open_close_count(self, hist_data):
+        if hist_data._name == "EUR":
+            return (
+                np.diff(
+                    hist_data.close.get(ago=0, size=self.p.eur_open_count)),
+                np.diff(
+                    hist_data.close.get(ago=0, size=self.p.eur_close_count)),
+                self.getposition(hist_data).size)
+        elif hist_data._name == "AUD":
+            return (
+                np.diff(
+                    hist_data.close.get(ago=0, size=self.p.aud_open_count)),
+                np.diff(
+                    hist_data.close.get(ago=0, size=self.p.aud_close_count)),
+                self.getposition(hist_data).size)
+        else:
+            return (
+                np.diff(
+                    hist_data.close.get(ago=0, size=self.p.gbp_open_count)),
+                np.diff(
+                    hist_data.close.get(ago=0, size=self.p.gbp_close_count)),
+                self.getposition(hist_data).size
+            )
 
-    def get_gbp_stopprice(self, close_price):
-        loss_per_one = self.p.gbp_stoploss / 300000
-        return close_price + loss_per_one
+    def set_order(self, hist_data):
+        bracket_sell = None
+        bracket_buy = None
+        open_count, close_count, pos = self.check_open_close_count(
+            hist_data)
+        # self.check_open_close_count(hist_data)
+# -------------------- Buy ON rise
+        if np.all(open_count > 0) and (not pos):
+            bracket_buy = self.buy_bracket(data=hist_data, size=300000,
+                                           price=hist_data.close[0],
+                                           exectype=bt.Order.Market,
+                                           limitprice=self.get_limitprice(
+                                               hist_data.close[0], hist_data._name),
+                                           stopprice=self.get_stopprice(
+                                               hist_data.close[0], hist_data._name)
+                                           )
+            self.log(
+                f"BUY Order On {hist_data._name}, on Price %.4f" % hist_data.close[0])
+# ----------------------Close ON Rise
+        elif np.all(close_count > 0) and pos:
+            if bracket_sell:
+                self.cancel(bracket_sell[1])
+                self.cancel(bracket_sell[2])
+                bracket_sell = None
+            if bracket_buy:
+                self.cancel(self.bracket_buy[1])
+                self.cancel(self.bracket_buy[2])
+                bracket_buy = None
+            self.close(data=hist_data)
+            self.log(
+                f"CLOSE Order On {hist_data._name}, %.4f" % hist_data.close[0])
+#  -----------------------------Sell ON fall
+        if np.all(open_count < 0) and (not pos):
+            bracket_sell = self.sell_bracket(data=hist_data, size=-300000,
+                                             price=hist_data.close[0],
+                                             exectype=bt.Order.Market,
+                                             stopprice=self.get_limitprice(
+                                                 hist_data.close[0], hist_data._name),
+                                             limitprice=self.get_stopprice(
+                                                 hist_data.close[0], hist_data._name)
+                                             )
+            self.log(
+                f"SELL Order On {hist_data._name}, on Price %.4f" % hist_data.close[0])
+# Close on fall
+        elif np.all(close_count < 0) and pos:
+            if bracket_sell:
+                self.cancel(bracket_sell[1])
+                self.cancel(bracket_sell[2])
+                bracket_sell = None
+            if bracket_buy:
+                self.cancel(self.bracket_buy[1])
+                self.cancel(self.bracket_buy[2])
+                bracket_buy = None
+            self.close(data=hist_data)
+            self.log(
+                f"CLOSE Order On {hist_data._name}, %.4f" % hist_data.close[0])
 
     def next(self):
-
         self.counter += 1
         if self.counter < 10:
             return
 
-
-# -----------------------------------------------------------EUR/USD-----------------------------------------------
-        check_open_count_eur = np.diff(self.close_eur.get(ago=0, size=10))
-        check_close_count_eur = np.diff(self.close_eur.get(ago=0, size=5))
-        self.eur_pos = self.getposition(self.eur).size
-
-        if np.all(check_open_count_eur > 0) and (not self.eur_pos):
-            self.eur_bracket_buy = self.buy_bracket(data=self.eur, size=300000,
-                                                    price=self.close_eur[0],
-                                                    limitprice=self.get_eur_limitprice(
-                                                        self.close_eur[0]),
-                                                    stopprice=self.get_eur_stopprice(
-                                                        self.close_eur[0])
-                                                    )
-            self.log(
-                f"BUY on  pos = {self.eur_pos} On EUR/USD, %.5f" % self.close_eur[0])
-        elif np.all(check_close_count_eur > 0) and self.eur_pos < 0:
-            if self.eur_bracket_sell:
-                self.cancel(self.eur_bracket_sell[1])
-                self.cancel(self.eur_bracket_sell[2])
-            self.close(data=self.eur)
-            self.log(
-                f"CLOSE  pos = {self.eur_pos} On EUR/USD, %.5f" % self.close_eur[0])
-        if np.all(check_open_count_eur < 0) and (not self.eur_pos):
-            self.eur_bracket_sell = self.sell_bracket(data=self.eur, size=-300000,
-                                                      price=self.close_eur[0],
-                                                      stopprice=self.get_eur_limitprice(
-                                                          self.close_eur[0]),
-                                                      limitprice=self.get_eur_stopprice(
-                                                          self.close_eur[0])
-                                                      )
-            self.log(
-                f"SELL on  pos = {self.eur_pos} On EUR/USD, %.5f" % self.close_eur[0])
-        elif np.all(check_close_count_eur > 0) and self.eur_pos > 0:
-            if self.eur_bracket_buy:
-                self.cancel(self.eur_bracket_buy[1])
-                self.cancel(self.eur_bracket_buy[2])
-            self.close(data=self.eur)
-            self.log(
-                f"CLOSE  pos = {self.eur_pos} On EUR/USD, %.5f" % self.close_eur[0])
+        for hist_data in self.datas:
+            # print(hist_data.close[0])
+            self.set_order(hist_data)
 
 
-# ----------------------------------------------------------GBP/USD--------------------------------------------------------
-        check_open_count_gbp = np.diff(self.close_gbp.get(ago=0, size=10))
-        check_close_count_gbp = np.diff(self.close_gbp.get(ago=0, size=5))
-        self.gbp_pos = self.getposition(self.gbp).size
+def report_writer(mdd):
+    values = np.array(list(PROFIT_LOSS_DICT.values()))
+    with open(r"reports/strategy_report.txt", 'w') as r:
+        r.write("profit and los : \n")
+        for key, value in PROFIT_LOSS_DICT.items():
+            r.write(f"{key, value}")
+            r.write("\n")
 
-        if np.all(check_open_count_gbp < 0) and (not self.gbp_pos):
-            self.gbp_bracket_sell = self.sell_bracket(data=self.gbp, size=-300000,
-                                                      price=self.close_gbp[0],
-                                                      stopprice=self.get_gbp_limitprice(
-                                                          self.close_gbp[0]),
-                                                      limitprice=self.get_gbp_stopprice(
-                                                          self.close_gbp[0])
-                                                      )
-            self.log(
-                f"SELL on  pos = {self.gbp_pos} On GBP/USD, %.5f" % self.close_gbp[0])
-        elif np.all(check_close_count_gbp > 0) and self.gbp_pos > 0:
-            if self.gbp_bracket_buy:
-                self.cancel(self.gbp_bracket_buy[1])
-                self.cancel(self.gbp_bracket_buy[2])
-            self.close(data=self.gbp)
-            self.log(
-                f"CLOSE  pos = {self.gbp_pos} On GBP/USD, %.5f" % self.close_gbp[0])
-        elif np.all(check_open_count_gbp > 0) and (not self.gbp_pos):
-            self.gbp_bracket_buy = self.buy_bracket(data=self.gbp, size=300000,
-                                                    price=self.close_gbp[0],
-                                                    limitprice=self.get_gbp_limitprice(
-                                                        self.close_gbp[0]),
-                                                    stopprice=self.get_gbp_stopprice(
-                                                        self.close_gbp[0])
-                                                    )
-            self.log(
-                f"BUY on  pos = {self.gbp_pos} On GBP/USD, %.5f" % self.close_gbp[0])
-        elif np.all(check_close_count_gbp > 0) and self.gbp_pos < 0:
-            if self.gbp_bracket_sell:
-                self.cancel(self.gbp_bracket_sell[1])
-                self.cancel(self.gbp_bracket_sell[2])
-            self.close(data=self.gbp)
-            self.log(
-                f"CLOSE  pos = {self.gbp_pos} On GBP/USD, %.5f" % self.close_[0])
-
-# ----------------------------------------------------------AUD/USD--------------------------------------------------------
-        check_open_count_aud = np.diff(self.close_aud.get(ago=0, size=10))
-        check_close_count_aud = np.diff(self.close_aud.get(ago=0, size=5))
-        self.aud_pos = self.getposition(self.aud).size
-
-        if np.all(check_open_count_aud < 0) and (not self.aud_pos):
-            self.aud_bracket_sell = self.sell_bracket(data=self.aud, size=-300000,
-                                                      price=self.close_aud[0],
-                                                      stopprice=self.get_aud_limitprice(
-                                                          self.close_aud[0]),
-                                                      limitprice=self.get_aud_stopprice(
-                                                          self.close_aud[0])
-                                                      )
-            self.log(
-                f"SELL on  pos = {self.aud_pos} On EUR/USD, %.5f" % self.close_aud[0])
-        elif np.all(check_close_count_aud < 0) and self.aud_pos > 0:
-            if self.aud_bracket_buy:
-                self.cancel(self.aud_bracket_buy[1])
-                self.cancel(self.aud_bracket_buy[2])
-            self.close(data=self.aud)
-            self.log(
-                f"CLOSE  pos = {self.aud_pos} On AUD/USD, %.5f" % self.close_aud[0])
-        elif np.all(check_open_count_aud > 0) and (not self.aud_pos):
-            self.aud_bracket_buy = self.buy_bracket(data=self.aud, size=300000,
-                                                    price=self.close_aud[0],
-                                                    limitprice=self.get_aud_limitprice(
-                                                        self.close_aud[0]),
-                                                    stopprice=self.get_aud_stopprice(
-                                                        self.close_aud[0])
-                                                    )
-            self.log(
-                f"BUY on  pos = {self.aud_pos} On AUD/USD, %.5f" % self.close_aud[0])
-        elif np.all(check_close_count_aud > 0) and self.aud_pos < 0:
-            if self.aud_bracket_sell:
-                self.cancel(self.aud_bracket_sell[1])
-                self.cancel(self.aud_bracket_sell[2])
-            self.close(data=self.aud)
-            self.log(
-                f"CLOSE  pos = {self.aud_pos} On AUD/USD, %.5f" % self.close_aud[0])
+        r.write("Avg PnL : ")
+        r.write(f"{np.mean(values)}")
+        r.write("\n")
+        r.write("Std PnL : ")
+        r.write(f"{np.std(values)}")
+        r.write("\n")
+        r.write("MaxDrawDown : ")
+        r.write(f"{mdd}")
 
 
 def start():
@@ -244,6 +221,8 @@ def start():
 
     aud_df = pd.read_csv(AUD_MID_PATH, index_col=0, parse_dates=True)
     # print(aud_df.head())
+    for date in aud_df.index.date:
+        PROFIT_LOSS_DICT[date.__str__()] = 0
     aud_data = bt.feeds.PandasDirectData(
 
         dataname=aud_df,
@@ -253,7 +232,7 @@ def start():
     )
 
     eur_df = pd.read_csv(EUR_MID_PATH, index_col=0, parse_dates=True)
-    # print(eur_df.head())
+    print(eur_df.head())
     eur_data = bt.feeds.PandasDirectData(
 
         dataname=eur_df,
@@ -302,19 +281,19 @@ def start():
     }
     # Add MyStrat to Cerebro
 
-    cb.addstrategy(MyStrategy)
+    cb.addstrategy(MyStrategy, strat_params=strat_params)
 
     # Add comission and margin to Cerebro
     # cb.broker.setcommission(commission=2.42, margin=8600.0,
     #                         mult=1000.0, name='Light_Oil')
+    cb.addanalyzer(bt.analyzers.DrawDown, _name='mydrawndown')
+# print('Starting Portfolio Value: %.2f' % cb.broker.getvalue())
 
-    # print('Starting Portfolio Value: %.2f' % cb.broker.getvalue())
+    strat = cb.run()
 
-    cb.run()
+# print('Final Portfolio Value: %.2f' % cb.broker.getvalue())
 
-    # print('Final Portfolio Value: %.2f' % cb.broker.getvalue())
-
-    # log_repot()
+    report_writer(strat[0].analyzers.mydrawndown.get_analysis().max.moneydown)
 
 
 if __name__ == '__main__':
